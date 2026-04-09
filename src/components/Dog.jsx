35451da28f50653cb4c7d2f420d4f704b95dfa54
RihanC <crihan082@gmail.com>
2026-04-09 09:20:27 -0700

    added animation and applied map to branches

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { use } from 'react'
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { useThree } from '@react-three/fiber'
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture , useAnimations} from '@react-three/drei'
 import { normalMap } from 'three/tsl'
+import { useEffect } from 'react'
 
 const Dog = () => {
 
@@ -18,23 +19,48 @@ const Dog = () => {
 
     })
 
+    const { actions } = useAnimations(model.animations, model.scene)
 
-    const texture = useTexture({
-        normalMap: "/models/dog_normals.jpg",
-        sampleMatCap : "/matcap/mat-19.jpg"
-    }, (texture)=>{
+    useEffect(() => {
+        actions["Take 001"].play()
+    }, [actions])
 
+
+    // const texture = useTexture({
+    //     normalMap: "/models/dog_normals.jpg",
+    //     sampleMatCap : "/matcap/mat-19.jpg"
+    // }, (texture)=>{
+
+    // })
+
+    const[normalMap, sampleMatCap, branchMap, branchNormalMap] = useTexture([
+        "/models/dog_normals.jpg",
+        "/matcap/mat-19.jpg",
+        "/models/branches_diffuse.jpeg",
+        "/models/branches_normals.jpeg"
+    ]).map((texture) => {
+        texture.flipY = false
+        texture.colorSpace = THREE.SRGBColorSpace
+        return texture
     })
 
-    texture.normalMap.flipY = false
-    texture.sampleMatCap.colorSpace = THREE.SRGBColorSpace
+    //const normalMap = texture.normalMap
+
+    const branchMaterial = new THREE.MeshMatcapMaterial({
+        normalMap: branchNormalMap,
+        matcap: branchMap
+    })
+
 
     model.scene.traverse((child) => {
         if(child.name.includes("DOG")){
             child.material = new THREE.MeshMatcapMaterial({
-                normalMap: texture.normalMap,
-                matcap: texture.sampleMatCap
+                normalMap: normalMap,
+                matcap: sampleMatCap
             })
+        }
+        else{
+            child.material = branchMaterial
         }
     })
 
